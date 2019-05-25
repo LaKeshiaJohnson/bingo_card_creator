76e6af9d236890193b6aa3900c98600a99787854
lakeshia <ljohnson2k@gmail.com>
2019-05-25 16:14:21 -0500

    adjusted size of card to make nums fit

--- bcc.rb
+++ bcc.rb
@@ -1,7 +1,15 @@
+def align(num)
+  if num < 10
+    "  #{num}"
+  else
+    " #{num}"
+  end
+end
+
 #for a range of numbers .. is inclusive (1- 15) 
 #... is exclusive (1 - 15 but not including the number 15)
 #.to_a converts the range to an array
-#.sample() retieves a random amount from the array (takes an option argument for the amount to retrieve)
+#.sample() retieves a random amount from the array (takes an optional argument for the amount to retrieve)
 b_col = (1..15).to_a.sample(5)
 i_col = (16..30).to_a.sample(5)
 n_col = (31..45).to_a.sample(4)
@@ -15,18 +23,19 @@ puts g_col.inspect
 puts o_col.inspect 
 
 bingo_card = <<-BINGO
-+---+---+---+---+---+
-| B | I | N | G | O |
-+---+---+---+---+---+
-| #{b_col[0]} | #{i_col[0]} | #{n_col[0]} | #{g_col[0]} | #{o_col[0]} |
-+---+---+---+---+---+
-| #{b_col[1]} | #{i_col[1]} | #{n_col[1]} | #{g_col[1]} | #{o_col[1]} |
-+---+---+---+---+---+
-| #{b_col[2]} | #{i_col[2]} | F | #{g_col[2]} | #{o_col[2]} |
-+---+---+---+---+---+
-| #{b_col[3]} | #{i_col[3]} | #{n_col[2]} | #{g_col[3]} | #{o_col[3]} |
-+---+---+---+---+---+
-| #{b_col[4]} | #{i_col[4]} | #{n_col[3]} | #{g_col[4]} | #{o_col[4]} |
-+---+---+---+---+---+
++-----+-----+-----+-----+-----+
+|  B  |  I  |  N  |  G  |  O  |
++-----+-----+-----+-----+-----+
+| #{align(b_col[0])} | #{align(i_col[0])} | #{align(n_col[0])} | #{align(g_col[0])} | #{align(o_col[0])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[1])} | #{align(i_col[1])} | #{align(n_col[1])} | #{align(g_col[1])} | #{align(o_col[1])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[2])} | #{align(i_col[2])} |  F  | #{align(g_col[2])} | #{align(o_col[3])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[3])} | #{align(i_col[3])} | #{align(n_col[2])} | #{align(g_col[3])} | #{align(o_col[3])} |
++-----+-----+-----+-----+-----+
+| #{align(b_col[4])} | #{align(i_col[4])} | #{align(n_col[3])} | #{align(g_col[4])} | #{align(o_col[4])} |
++-----+-----+-----+-----+-----+
+
 BINGO
 puts bingo_card
